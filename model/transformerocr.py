1677055d1d96b272e0daa06664a2298af46e2a20
hungtv <hungtv@aiacademy.edu.vn>
2025-11-19 13:33:13 +0700

    Refactor model architecture and update configuration for OCR. Replaced LanguageTransformer with Seq2Seq in VietOCR class. Updated image resizing method to use LANCZOS filter. Added new ONNX session creation logic with GPU fallback. Removed deprecated configuration file and added new model path in Converter.ipynb. Updated .gitignore to include new model directory.

--- model/transformerocr.py
+++ model/transformerocr.py
@@ -1,5 +1,5 @@
 from model.backbone.cnn import CNN
-from model.seqmodel.transformer import LanguageTransformer
+from model.seqmodel.seq2seq import Seq2Seq
 from torch import nn
 
 class VietOCR(nn.Module):
@@ -12,21 +12,18 @@ class VietOCR(nn.Module):
         
         self.cnn = CNN(backbone, **cnn_args)
         self.seq_modeling = seq_modeling
-
-        self.transformer = LanguageTransformer(vocab_size, **transformer_args)
+        self.transformer = Seq2Seq(vocab_size, **transformer_args)
 
 
     def forward(self, img, tgt_input, tgt_key_padding_mask):
-            """
-            Shape:
-                - img: (N, C, H, W)
-                - tgt_input: (T, N)
-                - tgt_key_padding_mask: (N, T)
-                - output: b t v
-            """
-            src = self.cnn(img)
+        """
+        Shape:
+            - img: (N, C, H, W)
+            - tgt_input: (T, N)
+            - tgt_key_padding_mask: (N, T)
+            - output: b t v
+        """
+        src = self.cnn(img)
+        outputs = self.transformer(src, tgt_input)
 
-            outputs = self.transformer(
-                src, tgt_input, tgt_key_padding_mask=tgt_key_padding_mask
-            )
-            return outputs
+        return outputs
